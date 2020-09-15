063e3f00d8b6b642ecb809327c96238e1159af1c
Jam Risser <jam@codejam.ninja>
2020-09-15 20:42:00 +0000

    Finished basic docker

--- example/docker/latest/entrypoint.sh
+++ example/docker/latest/entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-BABEL_NODE="node_modules/.bin/babel-node"
-PRISMA="node_modules/.bin/prisma"
+BABEL_NODE="../node_modules/.bin/babel-node"
+GENERATE_PRISMA="../node_modules/.bin/generate-prisma"
+PRISMA="../node_modules/.bin/prisma"
+WAIT_FOR_POSTGRES="../node_modules/.bin/wait-for-postgres"
 
-sh prisma/scripts/generate.sh
-sh prisma/scripts/wait-for-postgres.sh
+cd prisma
+$GENERATE_PRISMA ..
+$WAIT_FOR_POSTGRES
 $PRISMA migrate up --experimental
 $BABEL_NODE --extensions '.ts,.tsx' /opt/app/prisma/seed.ts
+cd ..
 
-exec node /opt/app/dist/src/main
+exec node /opt/app/dist/main
--- example/prisma/seed.ts
+++ example/prisma/seed.ts
@@ -1,5 +1,8 @@
 import { seed } from 'prisma-scripts';
+import dotenv from 'dotenv';
+import path from 'path';
 
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 const { env } = process;
 
 (async () => {
