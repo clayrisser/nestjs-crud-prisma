a0d347e7e04c3b8d249ea06760f0ef9fd737c88d
Ravi Ram <ravi@nuevesolutions.com>
2020-09-17 01:41:50 -0500

    fixed service

--- example/src/modules/user/user.service.ts
+++ example/src/modules/user/user.service.ts
@@ -6,9 +6,6 @@ import { User } from '../../generated/type-graphql';
 @Injectable()
 export class UserService extends PrismaCrudService<User> {
   constructor(prisma: PrismaService) {
-    const result = await prisma.user.findMany();
-    result.length;
-    const count = await prisma.user.count();
     super(prisma, User);
   }
 }
