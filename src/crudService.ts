6d7be643bfde3eac2edc94d6222eff5695344300
Jam Risser <jam@codejam.ninja>
2020-09-17 01:09:37 +0000

    Updated crud service

--- src/crudService.ts
+++ src/crudService.ts
@@ -1,18 +1,3 @@
-import {
-  CrudService as NestJSXCrudService,
-  CrudRequestOptions
-} from '@nestjsx/crud';
-import { ParsedRequestParams } from '@nestjsx/crud-request';
+import { CrudService as NestJSXCrudService } from '@nestjsx/crud';
 
-export default abstract class CrudService<T> extends NestJSXCrudService<T> {
-  decidePagination(
-    parsed: ParsedRequestParams,
-    options: CrudRequestOptions
-  ): boolean {
-    return (
-      options.query?.alwaysPaginate ||
-      ((Number.isFinite(parsed.page) || Number.isFinite(parsed.offset)) &&
-        !!this.getTake(parsed, options.query!))
-    );
-  }
-}
+export default abstract class CrudService<T> extends NestJSXCrudService<T> {}
