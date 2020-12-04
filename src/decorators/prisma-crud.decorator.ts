33d03f72b7fe3b6018d890456b0837b1a45a674d
Ravi Kallepalli <ravi@austinwebdeveloper.co>
2020-12-04 09:50:06 +0000

    Addded crud class

--- src/decorators/prisma-crud.decorator.ts
+++ src/decorators/prisma-crud.decorator.ts
@@ -3,5 +3,5 @@ import { PrismaCrudRoutesFactory } from '../prisma-crud-routes.factory';
 
 export const PrismaCrud = (options: CrudOptions) => (target: Object) => {
   const factory = PrismaCrudRoutesFactory.create(target, options);
-  // factory = undefined;
+  factory = undefined;
 };
